71a55e2c97a91d2598a7d948db102ddcffbdc61d
saeed <dev.devverx@gmail.com>
2025-09-25 22:32:58 +0500

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { useAuth } from "./context/AuthContext";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./Pages/HomePage";
+import Header from "./components/Header";
+import Properties from "./Pages/Properties";
+import Booking from "./Pages/Booking";
+import Payment from "./Pages/Payment";
+import Footer from "./components/Footer";
+import ViewBooking from "./Pages/ViewBooking";
+import Login from "./auth/Login";
+import Signup from "./auth/Signup";
+import ProtectedRoute from "./components/ProtectedRoute";
+function App() {
+  const { user } = useAuth();
+  return (
+    <>
+      {/* Header always visible */}
+      {/* {user && <Header />}
+      <div className={user ? "pt-16" : ""}>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/properties/:id"
+          element={
+            <ProtectedRoute>
+              <Properties />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/booking/:id"
+          element={
+            <ProtectedRoute>
+              <Booking />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/payment/:id"
+          element={
+            <ProtectedRoute>
+              <Payment />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/viewbooking/:id"
+          element={
+            <ProtectedRoute>
+              <ViewBooking />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+     {user && <Footer />} */}
+
+      <Header />
+      <div className="pt-16">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/properties/:id" element={<Properties />} />
+          <Route path="/booking/:id" element={<Booking />} />
+          <Route path="/payment/:id" element={<Payment />} />
+          <Route path="/viewbooking/:id" element={<ViewBooking />} />
+        </Routes>
+      </div>
+       <Footer />
+    </>
+  );
+}
+
+export default App;
+
+
